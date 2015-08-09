5e8b93c7e14512bf2083565862ae28ab79ff60c3
tptryan <tpt.ryan@googlemail.com>
2015-08-08 22:30:28 -0700

    CP23 Assignment Animate with Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,13 +2,15 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
     
-    var revealPoint = function () {
-        for (var i = 0; i < points.length; i++ ) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";  
-        }
+    var pointsProperties = function(element) {
+        element.style.opacity = 1;
+        element.style.transform = "scaleX(1) translateY(0)";
+        element.style.msTransform = "scaleX(1) translateY(0)";
+        element.style.WebkitTransform = "scaleX(1) translateY(0)";  
+    }
+    
+    var revealPoint = function() {
+        forEach(points, pointsProperties)
     };
     
     revealPoint();
---
+++ scripts/utilities.js
@@ -0,0 +1,4 @@
+var forEach = function(array, action) {
+    for (var i = 0; i < array.length; i++)
+        action(array[i]);
+};
